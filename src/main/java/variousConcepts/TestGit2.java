c634161b224b0913d8458c1fa7e851f9360ed474
imajaeroh <83259128+imajaeroh@users.noreply.github.com>
2021-06-13 06:21:31 -0500

    3rd commit

---
+++ src/main/java/variousConcepts/TestGit2.java
@@ -0,0 +1,15 @@
+package variousConcepts;
+
+public class TestGit2 {
+	
+	public static void main(String[] args) {
+		printMe1();
+		
+	}
+
+	public static void printMe1() {
+		System.out.println("Test Git");
+	}
+
+
+}
