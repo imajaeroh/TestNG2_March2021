af8912ebe948841f348e52975b45c30460db57e5
imajaeroh <83259128+imajaeroh@users.noreply.github.com>
2021-06-13 06:41:46 -0500

    4th commit

--- src/main/java/variousConcepts/TestGit2.java
+++ src/main/java/variousConcepts/TestGit2.java
@@ -1,14 +1,20 @@
 package variousConcepts;
 
 public class TestGit2 {
-	
 	public static void main(String[] args) {
 		printMe1();
-		
+		printMe2();
+		printMe();
+		}
+	public static void printMe() {
+		System.out.println("Test Git");
 	}
-
 	public static void printMe1() {
 		System.out.println("Test Git");
+	}
+	
+	public static void printMe2() {
+		System.out.println("Test Git2");
 	}
 
 
